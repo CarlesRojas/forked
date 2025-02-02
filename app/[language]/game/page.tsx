d1174349a5264448d2b28c0fb85c89c3bee90927
CarlesRojas <carlesrojas@outlook.com>
2025-02-02 12:50:15 +0100

    almost

--- app/[language]/game/page.tsx
+++ app/[language]/game/page.tsx
@@ -4,7 +4,6 @@ import { PageProps } from "@/app/[language]/layout";
 import Board from "@/component/Board";
 import EvaluationBar from "@/component/EvaluationBar";
 import GameStatus from "@/component/GameStatus";
-import { AspectRatio } from "@/component/ui/aspect-ratio";
 import { ChessBoard } from "@/game/chess/ChessBoard";
 import { useStockfish } from "@/game/chess/stockfish/useStockfish";
 import { currentMatchAtom, savedChessboardAtom } from "@/state/game";
@@ -27,7 +26,7 @@ const Game = ({ params }: PageProps) => {
     if (!match) return;
 
     return (
-        <div className="relative grid h-full max-h-full w-full grid-cols-[min-content_minmax(0,1fr)_min-content] grid-rows-1 gap-6 p-6 portrait:grid-cols-1 portrait:grid-rows-[min-content_1fr_max-content]">
+        <div className="relative grid h-full max-h-full w-full grid-cols-[auto_1fr_auto] grid-rows-1 gap-6 p-6 portrait:grid-cols-1 portrait:grid-rows-[auto_1fr_auto]">
             <div
                 className="relative size-full"
                 style={{ maxHeight: chessBoardSize.height, maxWidth: chessBoardSize.width }}
@@ -35,12 +34,17 @@ const Game = ({ params }: PageProps) => {
                 <EvaluationBar evaluation={evaluation} mateIn={mateIn} gameOver={chessBoard.gameOver} />
             </div>
 
-            <div className="relative size-full">
-                <div className="relative aspect-square max-h-full" ref={chessBoardRef}>
-                    <AspectRatio ratio={1}>
-                        <Board chessBoard={chessBoard} evaluation={{ evaluate, bestMove, isReady }} />
-                    </AspectRatio>
+            <div className="relative h-full w-full portrait:h-0 portrait:min-h-full">
+                <div
+                    className="relative aspect-square max-h-full bg-red-500 portrait:h-full portrait:max-h-[unset] portrait:max-w-full"
+                    ref={chessBoardRef}
+                >
+                    <Board chessBoard={chessBoard} evaluation={{ evaluate, bestMove, isReady }} />
                 </div>
+                {/* <div className="relative aspect-square max-h-full" ref={chessBoardRef}>
+                    <AspectRatio ratio={1}>
+                    </AspectRatio>
+                </div> */}
             </div>
 
             <div className="relative size-full" style={isPortrait ? {} : { maxHeight: chessBoardSize.height }}>
